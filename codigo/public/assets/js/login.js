573f62097f93581251b6b81373708666aeb8e0ab
Pedro Henrique c Quinellato <pedrokinell2020@gmail.com>
2024-10-25 17:10:35 -0300

    alterações nos arquivos

--- codigo/public/assets/js/login.js
+++ codigo/public/assets/js/login.js
@@ -11,9 +11,9 @@
 
 
 // Página inicial de Login
-const LOGIN_URL = "/modulos/login/login.html";
-let RETURN_URL = "/modulos/login/index.html";
-const API_URL = '/usuarios';
+const LOGIN_URL = "/codigo/public/modulos/login/login.html";
+let RETURN_URL = "/codigo/public/index.html";
+const API_URL = 'http://localhost:3000/usuarios';
 
 // Objeto para o banco de dados de usuários baseado em JSON
 var db_usuarios = {};
