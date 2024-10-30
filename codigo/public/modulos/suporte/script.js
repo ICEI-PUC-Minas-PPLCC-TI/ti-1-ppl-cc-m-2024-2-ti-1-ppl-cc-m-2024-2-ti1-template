bb1d46a2f4943ff8ea756dec085fc5ded6585d6a
malumanini <marialuizam.deoliveira@gmail.com>
2024-10-30 11:13:11 -0300

    Mensagem enviada pega dados do usuário logado

--- codigo/public/modulos/suporte/script.js
+++ codigo/public/modulos/suporte/script.js
@@ -1,13 +1,34 @@
+var usuarioCorrente = {};
+
+window.onload = () => {
+  usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+  if (usuarioCorrenteJSON) {
+      usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
+  }
+    fetch (`/usuarios?id=${usuarioCorrente.id}`);
+}
+
 (function () {
     emailjs.init("Ks27mHXoHCdkct1Vr");
 })();
 
 document.getElementById('feedbackForm').addEventListener('submit', function (event) {
     event.preventDefault(); 
+    var radios = document.getElementsByName('tipoAjuda');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            var tipo_radio = radios[i].value;
+            break;
+        }
+    }
 
     const templateParams = {
-        assunto: document.getElementById('assunto').value,
-        mensagem: document.getElementById('mensagem').value
+        message_type: tipo_radio,
+        from_name: usuarioCorrente.nome,
+        user_email: usuarioCorrente.email,
+        subject: document.getElementById("assunto").value,
+        message: document.getElementById("mensagem").value
     };
 
     emailjs.send('service_m6l7euo', 'template_5r0s015', templateParams)
@@ -16,5 +37,7 @@ document.getElementById('feedbackForm').addEventListener('submit', function (eve
         }, function (error) {
             alert('Erro ao enviar feedback: ' + JSON.stringify(error));
         });
+        document.getElementById("assunto").value = "";
+        document.getElementById("mensagem").value = "";
 });
 
