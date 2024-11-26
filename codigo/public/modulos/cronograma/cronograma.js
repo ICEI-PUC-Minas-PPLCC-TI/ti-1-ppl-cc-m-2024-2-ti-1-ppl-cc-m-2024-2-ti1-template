6dacf623ef675eb7e4fd5895a83c455741c16d11
Marcosware <marcosware.dev@gmail.com>
2024-11-26 08:55:48 -0300

    Cronograma update

--- codigo/public/modulos/cronograma/cronograma.js
+++ codigo/public/modulos/cronograma/cronograma.js
@@ -14,8 +14,6 @@ function execucao() {
           .then (response => response.json())
           .then (data => {
               carregarCronograma(data);
-              
-              // createPieChart(data);
           })
           .catch (error => {
               alert ('Erro ao obter dados do servidor:' + error.message);
@@ -23,6 +21,7 @@ function execucao() {
   }
 
 window.onload = () => {
+    document.getElementById('btn_salvar').addEventListener('click', criarHorario);  
     execucao();
     /* fetch (`/cronograma?id=${usuarioCorrente.id}`)
     .then (response => response.json())
@@ -67,23 +66,133 @@ function tarefaModal(dataTasks) {
 
 }
 
-function criarHorario(dataTasks) {
-
+function valorCheckbox() {
+    let checkboxes = document.getElementsByName('checkSem');
+    const checkValues = [];
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            checkValues.push(checkboxes[i].value);
+        }
+    }
+    return checkValues;
 }
 
+function montarHorario(tarefa, diasCheck, horasInicio, minutosInicio, horasFim, minutosFim) {
+    let novoHorario;
+    fetch(`/cronograma?id=${usuarioCorrente.id}`, {
+        method: 'GET',
+        headers: {
+            'Cache-Control': 'no-cache', // Impede o cache
+        }
+    })
+      .then (response => response.json())
+      .then (data => {
+        let idsAnt = data.flatMap(item => item.tarefas.ids)
+        if (idsAnt.includes(tarefa)) {
+            return;
+        }
+        idsAnt.push(parseInt(tarefa))
+        alert(idsAnt);
+        let horariosInicio = data.flatMap(item => item.tarefas.horarios.inicio)
+        let inicio = {
+            hr: parseInt(horasInicio),
+            min: parseInt(minutosInicio)
+        }
+        horariosInicio.push(inicio)
+        let horariosFim = data.flatMap(item => item.tarefas.horarios.fim)
+        let fim = {
+            hr: parseInt(horasFim),
+            min: parseInt(minutosFim)
+        }
+        horariosFim.push(fim)
+        let diasDaSemana = data.flatMap(item => item.tarefas.horarios.diasSemana)
+        diasCheck = diasCheck.map(Number);
+        let novosDias = {
+            dias: diasCheck
+        }
+        diasDaSemana.push(novosDias);
+        novoHorario = {
+            ids: idsAnt,
+            horarios:{
+                inicio: horariosInicio,
+                fim: horariosFim,
+                diasSemana: diasDaSemana
+            }
+        }
+        alert(JSON.stringify(novoHorario));
+      })
+      .catch (error => {
+          alert ('Erro ao obter dados do servidor 2:' + error.message);
+        })
+    return novoHorario;
+}
+
+function inserirHorario(novoHorario) {
+    fetch(`/cronograma?id=${usuarioCorrente.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+        },
+        body: JSON.stringify(novoHorario),
+    })
+        .then(response => response.json())
+        .then(data => {
+                alert("Novo horário inserido com sucesso.");
+                execucao(); 
+        })
+        .catch(error => {
+            console.error('Erro ao atualizar cronograma via API JSONServer:', error);
+            alert("Erro ao atualizar cronograma");
+        });
+}
+
+function criarHorario() {
+    usuarioCorrenteJSON = sessionStorage.getItem('usuarioCorrente');
+    if (usuarioCorrenteJSON) {
+        usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
+    }
+        fetch(`/cronograma?id=${usuarioCorrente.id}`, {
+            method: 'GET',
+            headers: {
+                'Cache-Control': 'no-cache', // Impede o cache
+            }
+        })
+          .then (response => response.json())
+          .then (data => {
+            let tarefa = document.getElementById('tasks').value;
+            let horarioInicio = document.getElementById('horarioInicio').value;
+            let horarioFim = document.getElementById('horarioFim').value;
+            let diasCheck = valorCheckbox();
+            let horas = horarioInicio.split(":");
+            let horasInicio = horas[0];
+            let minutosInicio = horas[1];
+            horas = horarioFim.split(":");
+            let horasFim = horas[0];
+            let minutosFim = horas[1];
+            let novoHorario = montarHorario(tarefa, diasCheck, horasInicio, minutosInicio, horasFim, minutosFim);
+            inserirHorario(data, novoHorario);
+          })
+          .catch (error => {
+              alert ('Erro ao obter dados do servidor 3:' + error.message);
+          })
+    }
+
 function carregarTarefa(data, dataTasks, i) {
-    let horarios = Array.from(data.map(item => item.tarefas.horarios.inicio[i].hr + ":" + item.tarefas.horarios.inicio[i].min + " - "
-        + item.tarefas.horarios.fim[i].hr + ":" + item.tarefas.horarios.fim[i].min));
     let dias = Array.from(data.map(item => item.tarefas.horarios.diasSemana[i].dias));
-    // alert(dias);
+    //let horarios = Array.from(data.map(item => item.tarefas.horarios.inicio[i].hr + ":" + item.tarefas.horarios.inicio[i].min + " - "
+    //   + item.tarefas.horarios.fim[i].hr + ":" + item.tarefas.horarios.fim[i].min));
+    let horarios = Array.from(data.map(item => item.tarefas.horarios.inicio[i].hr + ":" + String(item.tarefas.horarios.inicio[i].min).padStart(2, '0') + " - "
+       + item.tarefas.horarios.fim[i].hr + ":" + String(item.tarefas.horarios.fim[i].min).padStart(2, '0')));
+    let nomes = Array.from(data.map(item => dataTasks.find(task => task.id === item.tarefas.ids[i])?.nomeTarefa));
+      
     
     // tarefaHTML.className = "col";
-    
     for(let i = 0; i < dias.length; i++) {
-
         dias[i].forEach(dia => {
             let tarefaHTML = document.createElement("div");
-            tarefaHTML.innerHTML = `${horarios.join(', ')}`;
+            let tarefaInfo = `${horarios[i]}\n${nomes[i]}`;
+            tarefaHTML.innerHTML = `${tarefaInfo}`;
             document.getElementById(dia).appendChild(tarefaHTML);
         })
     }
